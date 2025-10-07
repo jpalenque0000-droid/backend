f6e38dc34ddf6455e8bd52b95802cefaa582c582
Juan Palenque <jpalenque0000@gmail.com>
2025-10-07 12:41:14 -0500

    adding more capacity to json

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ import auth from './auth.js';
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/api', (req, res, next) => {
     if (req.path === '/login' || req.path === '/register' || req.path === "/usdt-price") {
